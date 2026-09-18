1e3f3cf60ec96473b76063704474cfede841ed8c
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-09-18 01:34:06 -0400

    Export stellar observables solver

--- src/ns_response_geometry/__init__.py
+++ src/ns_response_geometry/__init__.py
@@ -2,13 +2,16 @@
 
 from .eos import IncompressibleEOS, RelativisticPolytrope
 from .geometry import induced_response, metric_projector, transverse_response
+from .observables import StellarObservables, solve_observables
 from .tov import Star, solve_star
 
 __all__ = [
     "IncompressibleEOS",
     "RelativisticPolytrope",
     "Star",
+    "StellarObservables",
     "solve_star",
+    "solve_observables",
     "induced_response",
     "metric_projector",
     "transverse_response",
